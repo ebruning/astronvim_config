e7f50d9cde6ad5aab01ad8f095d1ea1da78f2d0e
Ethan Bruning <ethan@bruning.io>
2023-04-16 09:55:07 -0700

    add virt-column

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,7 +1,15 @@
 return {
   { 
     'Bekaboo/deadcolumn.nvim',
+    enabled = false,
     lazy = false,
+  },
+  {
+    "lukas-reineke/virt-column.nvim",
+    lazy = false,
+    config = function()
+      require("virt-column").setup()
+    end,
   },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
