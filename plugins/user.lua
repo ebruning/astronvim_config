2d37bc02bd7a6855c29544b217b9e36ed2e8e039
Ethan Bruning <ethan@fairmatic.com>
2023-04-17 15:02:03 -0700

    Refactor

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,18 +1,16 @@
 return {
   {
-    'Bekaboo/deadcolumn.nvim',
+    "Bekaboo/deadcolumn.nvim",
     enabled = false,
     lazy = false,
   },
   {
     "lukas-reineke/virt-column.nvim",
     lazy = false,
-    config = function()
-      require("virt-column").setup()
-    end,
+    config = function() require("virt-column").setup() end,
   },
   { "ellisonleao/gruvbox.nvim", enabled = false },
-  { "luisiacc/gruvbox-baby" }
+  { "luisiacc/gruvbox-baby" },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
