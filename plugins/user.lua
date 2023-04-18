ab2f3eb8f82258593df9f9128bfccefe94f9095f
Ethan Bruning <ethan@fairmatic.com>
2023-04-17 17:58:14 -0700

    remove unused code, should have been in init.lua

--- plugins/user.lua
+++ plugins/user.lua
@@ -26,46 +26,6 @@ return {
       }
     end,
   },
-  icons = {
-    VimIcon = "",
-    ScrollText = "",
-    GitBranch = "",
-    GitAdd = "",
-    GitChange = "",
-    GitDelete = "",
-  },
-  heirline = {
-    -- define the separators between each section
-    separators = {
-      left = { "", " " }, -- separator for the left side of the statusline
-      right = { " ", "" }, -- separator for the right side of the statusline
-      tab = { "", "" },
-    },
-    -- add new colors that can be used by heirline
-    colors = function(hl)
-      local get_hlgroup = require("astronvim.utils").get_hlgroup
-      -- use helper function to get highlight group properties
-      local comment_fg = get_hlgroup("Comment").fg
-      hl.git_branch_fg = comment_fg
-      hl.git_added = comment_fg
-      hl.git_changed = comment_fg
-      hl.git_removed = comment_fg
-      hl.blank_bg = get_hlgroup("Folded").fg
-      hl.file_info_bg = get_hlgroup("Visual").bg
-      hl.nav_icon_bg = get_hlgroup("String").fg
-      hl.nav_fg = hl.nav_icon_bg
-      hl.folder_icon_bg = get_hlgroup("Error").fg
-      return hl
-    end,
-    attributes = {
-      mode = { bold = true },
-    },
-    icon_highlights = {
-      file_icon = {
-        statusline = false,
-      },
-    },
-  },
   {
     "rebelot/heirline.nvim",
     opts = function(_, opts)
@@ -173,23 +133,6 @@ return {
           },
         },
       }
-      -- local status = require "astronvim.utils.status"
-      -- opts.statusline = {
-      --   -- statusline
-      --   hl = { fg = "fg", bg = "bg" },
-      --   status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-      --   status.component.git_branch(),
-      --   status.component.file_info { filetype = {}, filename = false, file_modified = false },
-      --   status.component.git_diff(),
-      --   status.component.diagnostics(),
-      --   status.component.fill(),
-      --   status.component.cmd_info(),
-      --   status.component.fill(),
-      --   status.component.lsp(),
-      --   status.component.treesitter(),
-      --   status.component.nav(),
-      --   -- remove the 2nd mode indicator on the right
-      -- }
 
       opts.winbar = {
         -- create custom winbar
