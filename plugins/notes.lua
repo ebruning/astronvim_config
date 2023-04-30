4c71e4e27f555f95f2ad0fceaa75bb4250251043
Ethan Bruning <ethan@bruning.io>
2023-04-29 18:36:48 -0700

    add note taking plugins

---
+++ plugins/notes.lua
@@ -0,0 +1,17 @@
+return {
+  { "AckslD/nvim-FeMaco.lua", ft = { "markdown" }, opts = {} },
+  {
+    "folke/twilight.nvim",
+    opts = {},
+    cmd = { "Twilight", "TwilightEnable", "TwilightDisable" },
+  },
+  { "folke/zen-mode.nvim", opts = {}, cmd = { "ZenMode" } },
+  { "dhruvasagar/vim-table-mode", ft = { "markdown", "org", "norg" } },
+  { "lukas-reineke/headlines.nvim", opts = {}, ft = { "markdown", "org", "norg" } },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    lazy = true,
+    build = "cd app && npm install && git reset --hard",
+  },
+}
