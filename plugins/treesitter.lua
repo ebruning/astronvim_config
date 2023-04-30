927e1ce06c56436b4ed84328587e498475b12da2
Ethan Bruning <ethan@bruning.io>
2023-04-29 18:01:08 -0700

    add highlight

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,5 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = { "lua", "org" },
+    highlight = { enable = true, additional_vim_regex_highlighting = { "org" } },
   },
 }
